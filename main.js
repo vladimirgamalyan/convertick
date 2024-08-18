14357c00563bdad6e377f5adeaafa51c0db340c6
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2024-08-18 15:35:57 +0700

    First MVP

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 window.onload = function() {
     function updateValue(e) {
-        textAreaOutput.textContent = e.target.value;
+        let utf8Encode = new TextEncoder();
+        let encoded = utf8Encode.encode(e.target.value);
+        let result = ''
+        for (let b of encoded) {
+            result += b.toString(2).padStart(8, "0");
+        }
+        textAreaOutput.textContent = result;
     }
 
     const textAreaInput = document.getElementById("textAreaInput");
